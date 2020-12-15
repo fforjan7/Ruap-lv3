f29686874a6d70957f329497f91bf54263bcf1a5
Filip Forjan <61835361+fforjan7@users.noreply.github.com>
2020-12-15 17:29:51 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -59,8 +59,8 @@ register.</p>
 // DB connection info
 // TODO: Update the values for $host, $user, $pwd, and $db //using the values you retrieved
 earlier from the Azure Portal. $host = "value of Data Source";
-$host = "ruapdbserver.mysql.database.azure.com";
-$user = "ruapUser@ruapdbserver";
+$host = "fforjan-mysqldbserver.mysql.database.azure.com";
+$user = "ruapUser@fforjan-mysqldbserver";
 $pwd = "Ruap1234";
 $db = "ruapdb";
 // Connect to database.
